011bab791c350f707ebdce1d81f8c0c2f7b1534a
buituankiet <buituankiet09102000@gmail.com>
2022-09-07 14:28:27 +0700

    feat(6): Create App service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
+import { LocalStrategy } from './local.strategy';
+import { AuthService } from './service/auth.service';
+import { Users } from './user.entity';
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60m' },
+    }),
+    TypeOrmModule.forFeature([Users]),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy]
+})
 export class AuthModule {}
