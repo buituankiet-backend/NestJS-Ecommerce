8a2a0d5075144d849ae763ba0311a81e52835bd4
buituankiet <buituankiet09102000@gmail.com>
2022-09-07 14:54:57 +0700

    fix 1

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Param, Request, Body, UseGuards } from '@nestjs/common';
+import {UpdateResult, DeleteResult} from 'typeorm';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { ProductsService } from './product.service';
+import { ProductEntity } from './product.entity';
 
-@Controller('product')
-export class ProductController {}
+@Controller('api/v1/products')
+export class ProductsController {
+ constructor(private productsService: ProductsService) { }
+
+ @UseGuards(JwtAuthGuard)
+ @Get()
+ async GetAll(): Promise<ProductEntity[]> {
+   return await this.productsService.getAll();
+
+ }
+
+ @UseGuards(JwtAuthGuard)
+ @Post()
+ async Create(@Request() req, @Body() product: ProductEntity): Promise<ProductEntity> {
+   return await this.productsService.create(product, req.user);
+ }
+
+
+ @UseGuards(JwtAuthGuard)
+ @Get(':id')
+ async GetOne(@Param() id: number): Promise<ProductEntity> {
+   return await this.productsService.getOne(id);
+
+ }
+
+ @UseGuards(JwtAuthGuard)
+ @Put(':id')
+ async Update(@Param() id: number, @Body() product: ProductEntity, @Request() req): Promise<UpdateResult> {
+   return await this.productsService.update(id, product, req.user);
+
+ }
+
+ @UseGuards(JwtAuthGuard)
+ @Delete(':id')
+ async Delete(@Param() id: number, @Request() req): Promise<DeleteResult> {
+   return await this.productsService.delete(id, req.user);
+
+ }
+}
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductEntity } from './product.entity';
 import { ProductsService } from './product.service';
-import { ProductController } from './product.controller';
+import { ProductsController } from './product.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProductEntity])],
   providers: [ProductsService],
-  controllers: [ProductController]
+  controllers: [ProductsController]
 })
 export class ProductModule {}
